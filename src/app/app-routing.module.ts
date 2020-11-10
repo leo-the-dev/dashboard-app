82d86e7c6f37543bde3984dbc0dc6bf6644e8879
leo <pol.leonardo2@gmail.com>
2020-11-10 20:44:09 +0100

    add sales, orders, health components; implement routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HealthComponent } from './health/health.component';
+import { OrdersComponent } from './orders/orders.component';
+import { SalesComponent } from './sales/sales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'sales', component : SalesComponent },
+  { path : 'orders', component : OrdersComponent },
+  { path : 'health', component : HealthComponent },
+  { path : '', redirectTo : '/sales', pathMatch : 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SalesComponent } from './sales/sales.component';
+import { OrdersComponent } from './orders/orders.component';
+import { HealthComponent } from './health/health.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    SalesComponent,
+    OrdersComponent,
+    HealthComponent
   ],
   imports: [
     BrowserModule,
