655e10a92229da0bc229d29686f441987c28fddb
leo <pol.leonardo2@gmail.com>
2020-11-10 22:03:02 +0100

    implement doughnut charts 😍

--- src/app/pie-chart/pie-chart.component.ts
+++ src/app/pie-chart/pie-chart.component.ts
@@ -9,6 +9,13 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  public pieChartData: number[] = [300, 450, 120];
+  public pieChartLabels: string[] = ['leo', 'pluto', 'pippo'];
+  public pieChartColors: any[] = [
+    { backgroundColor : ['#26547c', '#ff6b6b', '#ffd166'], borderColor : '#111'}
+  ];
+  pieChartType = 'doughnut';
+
   ngOnInit(): void {
   }
 
